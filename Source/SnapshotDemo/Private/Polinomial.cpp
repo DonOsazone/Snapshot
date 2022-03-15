12421174feea00d96c604400c93a0fc42693d21b
Gyss <donosazone@gmail.com>
2022-03-15 12:42:59 +0800

    useless

--- Source/SnapshotDemo/Private/Polinomial.cpp
+++ Source/SnapshotDemo/Private/Polinomial.cpp
@@ -10,7 +10,7 @@ Polinomial::Polinomial()
 Polinomial::~Polinomial()
 {
 }
-
+/*
 Polinomial operator+ (const Polinomial& p1, const Polinomial& p2) {
 	int p1length = p1.content.size();
 	int p2length = p2.content.size();
@@ -20,3 +20,4 @@ Polinomial operator+ (const Polinomial& p1, const Polinomial& p2) {
 			
 	}
 }
+*/
